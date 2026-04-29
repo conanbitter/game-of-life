3944120f03e11c98bd31d93ae5520e65dfae0532
Conan Bitter <conan.bitter@gmail.com>
2026-04-29 22:01:14 +0300

    Implement background prerender

--- main.lua
+++ main.lua
@@ -6,19 +6,28 @@ end
 local image = nil
 
 ---@type love.Quad
-local quad_bg = nil
-
----@type love.Quad
 local quad_cell = nil
 
 ---@type love.Quad
 local quad_frame = nil
 
+---@type love.Canvas
+local canvas_bg = nil
+
 function love.load()
     image = love.graphics.newImage("life.png")
-    quad_bg = love.graphics.newQuad(0, 0, 24, 24, image)
+    local quad_bg = love.graphics.newQuad(0, 0, 24, 24, image)
     quad_cell = love.graphics.newQuad(24, 0, 24, 24, image)
     quad_frame = love.graphics.newQuad(48, 0, 24, 24, image)
+
+    canvas_bg = love.graphics.newCanvas(SCREEN_WIDTH, SCREEN_HEIGHT)
+    love.graphics.setCanvas(canvas_bg)
+    for y = 0, GRID_HEIGHT - 1 do
+        for x = 0, GRID_WIDTH - 1 do
+            love.graphics.draw(image, quad_bg, x * CELL_SIZE, y * CELL_SIZE)
+        end
+    end
+    love.graphics.setCanvas()
 end
 
 function love.update(dt)
@@ -26,11 +35,7 @@ function love.update(dt)
 end
 
 function love.draw()
-    for y = 0, GRID_HEIGHT - 1 do
-        for x = 0, GRID_WIDTH - 1 do
-            love.graphics.draw(image, quad_bg, x * CELL_SIZE, y * CELL_SIZE)
-        end
-    end
+    love.graphics.draw(canvas_bg)
 
     love.graphics.draw(image, quad_cell, 24 * 3, 24)
 
